ca839bce852c2fa6055053b657c75c7b0e44f01e
akitodr <lara.muller@workmail.com>
2020-06-06 23:38:39 -0300

    image upload

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -18,7 +18,14 @@ class PointsController {
             .distinct()
             .select('points.*');
 
-        return response.json(points);
+        const serializedPoints = points.map(point => {
+            return {
+                ...point,
+                image_url: `http://192.168.100.13:3333/uploads/${point.image}`,
+            };
+        });
+
+        return response.json(serializedPoints);
 
     }
 
@@ -27,16 +34,21 @@ class PointsController {
 
         const point = await knex('points').where('id', id).first();
 
-        if(!point) {
+        if (!point) {
             return response.status(400).json({ message: 'Point not found.' });
         }
+
+        const serializedPoint = {
+            ...point,
+            image_url: `http://192.168.100.13:3333/uploads/${point.image}`,
+        };
 
         const items = await knex('items')
             .join('point_items', 'items.id', '=', 'point_items.item_id')
             .where('point_items.point_id', id)
             .select('items.title');
 
-        return response.json({ point, items });
+        return response.json({ point: serializedPoint, items });
     }
 
     async create(request: Request, response: Response) {
@@ -50,11 +62,11 @@ class PointsController {
             uf,
             items
         } = request.body;
-    
+
         const trx = await knex.transaction();
 
         const point = {
-            image: 'https://images.unsplash.com/photo-1556767576-aa7f4d92262b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+            image: request.file.filename,
             name,
             email,
             whatsapp,
@@ -63,26 +75,29 @@ class PointsController {
             city,
             uf
         };
-    
+
         const insertedIds = await trx('points').insert(point);
-    
+
         const point_id = insertedIds[0];
-    
-        const pointItems = items.map((item_id: number) => {
-            return {
-                item_id,
-                point_id,
-            };
-        });
-    
+
+        const pointItems = items
+            .split(',')
+            .map((item: string) => Number(item.trim()))
+            .map((item_id: number) => {
+                return {
+                    item_id,
+                    point_id,
+                };
+            });
+
         await trx('point_items').insert(pointItems);
-    
+
         await trx.commit();
 
-        return response.json({ 
+        return response.json({
             id: point_id,
             ...point,
-         });
+        });
     }
 }
 
--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -6,6 +6,8 @@ import axios from 'axios';
 import { LeafletMouseEvent } from 'leaflet';
 import api from '../../services/api';
 
+import Dropzone from '../../components/Dropzone';
+
 import './styles.css';
 import logo from '../../assets/logo.svg';
 import Home from '../Home';
@@ -43,6 +45,7 @@ const CreatePoint = () => {
     const [selectedCity, setSelectedCity] = useState('0');
     const [selectedItems, setSelectedItems] = useState<number[]>([]);
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+    const [selectedFile, setSelectedFile] = useState<File>();
 
     const history = useHistory();
 
@@ -50,7 +53,7 @@ const CreatePoint = () => {
         navigator.geolocation.getCurrentPosition(position => {
             const { latitude, longitude } = position.coords;
 
-            setInitialPosition([ latitude, longitude ]);
+            setInitialPosition([latitude, longitude]);
         });
     }, []);
 
@@ -102,33 +105,39 @@ const CreatePoint = () => {
     }
 
     function handleSelectItem(id: number) {
-        if(selectedItems.includes(id)) {
+        if (selectedItems.includes(id)) {
             const filteredItems = selectedItems.filter(item => item !== id);
             setSelectedItems(filteredItems);
         } else {
-            setSelectedItems([ ...selectedItems, id ]);
+            setSelectedItems([...selectedItems, id]);
         }
-        
+
     }
 
     async function handleSubmit(event: FormEvent) {
         event.preventDefault();
+
         const { name, email, whatsapp } = formData;
         const uf = selectedUf;
         const city = selectedCity;
         const [latitude, longitude] = selectedPosition;
         const items = selectedItems;
 
-        const data = {
-            name,
-            email,
-            whatsapp,
-            uf,
-            city,
-            latitude,
-            longitude,
-            items
-        };
+        const data = new FormData();
+
+        data.append('name', name);
+        data.append('email', email);
+        data.append('whatsapp', whatsapp);
+        data.append('uf', uf);
+        data.append('city', city);
+        data.append('latitude', String(latitude));
+        data.append('longitude', String(longitude));
+        data.append('items', items.join(','));
+        
+        if (selectedFile) {
+            data.append('image', selectedFile);
+        }
+
 
         await api.post('points', data);
 
@@ -150,6 +159,8 @@ const CreatePoint = () => {
 
             <form onSubmit={handleSubmit}>
                 <h1>Cadastro do <br /> ponto de coleta</h1>
+
+                <Dropzone onFileUploaded={setSelectedFile} />
 
                 <fieldset>
                     <legend>
@@ -194,11 +205,11 @@ const CreatePoint = () => {
                         <span>Selecione o endereço no mapa</span>
                     </legend>
 
-                    <Map 
-                        center={initialPosition} 
+                    <Map
+                        center={initialPosition}
                         zoom={15}
                         onClick={handleMapClick}
-                        >
+                    >
                         <TileLayer
                             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -224,12 +235,12 @@ const CreatePoint = () => {
                         </div>
                         <div className="field">
                             <label htmlFor="city">Cidade</label>
-                            <select 
-                                name="city" 
+                            <select
+                                name="city"
                                 id="city"
                                 value={selectedCity}
                                 onChange={handleSelectedCity}
-                                >
+                            >
                                 <option value="0">Selecione uma cidade</option>
                                 {cities.map(city => (
                                     <option key={city} value={city}>{city}</option>
@@ -247,8 +258,8 @@ const CreatePoint = () => {
 
                     <ul className="items-grid">
                         {items.map(item => (
-                            <li 
-                                key={item.id} 
+                            <li
+                                key={item.id}
                                 onClick={() => handleSelectItem(item.id)}
                                 className={selectedItems.includes(item.id) ? 'selected' : ''}
                             >
